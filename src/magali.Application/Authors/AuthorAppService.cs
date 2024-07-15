b41267c5158d750a9edbe022b699512d8064b52b
Fabian Serna <fabian.serna@upb.edu.co>
2024-07-15 18:34:51 -0500

    Apply filter in GetListAsync for Authors

--- src/magali.Application/Authors/AuthorAppService.cs
+++ src/magali.Application/Authors/AuthorAppService.cs
@@ -1,7 +1,11 @@
 ﻿using magali.Permissions;
 using Microsoft.AspNetCore.Authorization;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
 using System.Threading.Tasks;
+using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
 
@@ -38,6 +42,32 @@ namespace magali.Authors
             author = await Repository.InsertAsync(author);
 
             return ObjectMapper.Map<Author, AuthorDto>(author);
+        }
+
+        public override async Task<PagedResultDto<AuthorDto>> GetListAsync(PagedSortedAndFilteredResultRequestDto input)
+        {
+            await CheckGetListPolicyAsync();
+
+            var query = await Repository.GetQueryableAsync();
+
+            // Apply filtering
+            query = query.WhereIf(!input.Filter.IsNullOrEmpty(), a => a.Name.ToLower().Contains(input.Filter.ToLower()));
+
+            // Apply sorting
+            query = query.OrderBy(input.Sorting ?? "creationTime desc");
+
+            // Get totalCount before about pagination
+            var totalCount = await AsyncExecuter.CountAsync(query);
+
+            // Apply paging
+            query = query.PageBy(input.SkipCount, input.MaxResultCount);
+
+            // Execute the query and get the result
+            var authors = await AsyncExecuter.ToListAsync(query);
+
+            var authorsDtos = ObjectMapper.Map<List<Author>, List<AuthorDto>>(authors).ToList();
+
+            return new PagedResultDto<AuthorDto>(totalCount, authorsDtos);
         }
     }
 }
